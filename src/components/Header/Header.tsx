0dadc90c20f61e4d27c0e12865f6332458f416c9
Agustin David Benitez <98838718+Bezagus@users.noreply.github.com>
2025-08-19 17:13:35 -0300

    add contact

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,12 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center px-2 md:px-10 py-4 absolute top-0 w-svw">
       <Logo />
-      <button className="text-desktop-p border-[1px] px-6 md:px-10 py-2 max-h-[42px] rounded-full border-primary text-primary">
+      <a
+        href="mailto:help@bezlon.com"
+        className="text-desktop-p border-[1px] px-6 md:px-10 py-2 max-h-[42px] rounded-full border-primary text-primary flex items-center justify-center"
+      >
         Contacto
-      </button>
+      </a>
     </header>
   );
 };
